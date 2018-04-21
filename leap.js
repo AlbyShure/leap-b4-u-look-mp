d2f6faf766d2aaa2dee6f1fc9eda89f69a0ad2e2
AlbyShure <allanh03@yahoo.com>
2018-04-21 00:58:22 -0500

    1st iteration
    Ported all elements except audio visualizer from the source website.

---
+++ leap.js
@@ -0,0 +1,245 @@
+alert("If you are on a smartphone or tablet, you may experience choppy areas or a non-animated background. This is because this website is not optimized for mobile use. You may also have to press the song buttons twice before listening. Thank You."
+)
+
+$('#play').on('click', function() {
+  $('audio')[0].play();
+});
+
+$('#alone').on('click', function() {
+  $('#audio-file').attr('src',
+'Alone.mp4');
+$('audio')[0].play();
+});
+
+$('#go').on('click', function() {
+  $('#audio-file').attr('src',
+'Let it Go Airhorn.mp4');
+$('audio')[0].play();
+});
+
+$('#trndsttr').on('click', function() {
+  $('#audio-file').attr('src',
+'Trndsttr.mp4');
+$('audio')[0].play();
+});
+
+$('#revolution').on('click', function() {
+  $('#audio-file').attr('src',
+'Revolution.mp4');
+$('audio')[0].play();
+});
+
+$('#hyperparadise').on('click', function() {
+  $('#audio-file').attr('src',
+'Hyperparadise.mp4');
+$('audio')[0].play();
+});
+
+$('#hyperreal').on('click', function() {
+  $('#audio-file').attr('src',
+'Hyperreal.mp4');
+$('audio')[0].play();
+});
+
+$('#warm').on('click', function() {
+  $('#audio-file').attr('src',
+'Warm Thoughts.mp4');
+$('audio')[0].play();
+});
+
+$('#animals').on('click', function() {
+  $('#audio-file').attr('src',
+'Animals.mp4');
+$('audio')[0].play();
+});
+
+$('#together').on('click', function() {
+  $('#audio-file').attr('src',
+'Be Together.mp4');
+$('audio')[0].play();
+});
+
+$('#lean').on('click', function() {
+  $('#audio-file').attr('src',
+'Lean On.mp4');
+$('audio')[0].play();
+});
+
+$('#ezra').on('click', function() {
+  $('#audio-file').attr('src',
+'Ezra.mp4');
+$('audio')[0].play();
+});
+
+$('#paper').on('click', function() {
+  $('#audio-file').attr('src',
+'Paper Thin.mp4');
+$('audio')[0].play();
+});
+
+$('#sleepless').on('click', function() {
+  $('#audio-file').attr('src',
+'Sleepless.mp4');
+$('audio')[0].play();
+});
+
+$('#pure').on('click', function() {
+  $('#audio-file').attr('src',
+'Pure.mp4');
+$('audio')[0].play();
+});
+
+$('#hearts').on('click', function() {
+  $('#audio-file').attr('src',
+'Hearts.mp4');
+$('audio')[0].play();
+});
+
+$('#wild').on('click', function() {
+  $('#audio-file').attr('src',
+'When I Get There.mp4');
+$('audio')[0].play();
+});
+
+$('#You').on('click', function() {
+  $('#audio-file').attr('src',
+'You n Me.mp4');
+$('audio')[0].play();
+});
+
+$('#aftergold').on('click', function() {
+  $('#audio-file').attr('src',
+'Aftergold.mp4');
+$('audio')[0].play();
+});
+
+$('#lost').on('click', function() {
+  $('#audio-file').attr('src',
+'Lost and Found.mp4');
+$('audio')[0].play();
+});
+
+$('#day').on('click', function() {
+  $('#audio-file').attr('src',
+'One Day Theyll Know.mp4');
+$('audio')[0].play();
+});
+
+$('#marijuana').on('click', function() {
+  $('#audio-file').attr('src',
+'Marijuana.mp4');
+$('audio')[0].play();
+});
+
+$('#world').on('click', function() {
+  $('#audio-file').attr('src',
+'The Next World.mp4');
+$('audio')[0].play();
+});
+
+$('#ocean').on('click', function() {
+  $('#audio-file').attr('src',
+'Ocean Floor Kisses.mp4');
+$('audio')[0].play();
+});
+
+$('#views').on('click', function() {
+  $('#audio-file').attr('src',
+'Perfect Views.mp4');
+$('audio')[0].play();
+});
+
+
+$('#pause').on('click', function() {
+  $('audio')[0].pause();
+});
+
+$('#kalimba').on('click', function() {
+  $('#audio-file').attr('src',
+'Kalimba.mp3');
+$('audio')[0].play();
+});
+
+$('#play-1').on('click', function() {
+  $('#audio-file').attr('src',
+'Song.wav');
+$('audio')[0].play();
+});
+
+$('#play-2').on('click', function() {
+  $('#audio-file').attr('src',
+'Song.mp3');
+$('audio')[0].play();
+});
+
+$('#play-3').on('click', function() {
+  $('#audio-file').attr('src',
+'Primo.mp3');
+$('audio')[0].play();
+});
+
+$('#play-4').on('click', function () {
+  $('#audio-file').attr('src',
+'Arazu.mp3');
+$('audio')[0].play();
+});
+
+$('#play-6').on('click', function () {
+  $('#audio-file').attr('src',
+'Lilypads.mp3');
+$('audio')[0].play();
+});
+
+$('#play-7').on('click', function () {
+  $('#audio-file').attr('src',
+'zZz.mp3');
+$('audio')[0].play();
+});
+
+$('#play-f').on('click', function () {
+  $('#audio-file').attr('src',
+'Fly.mp3');
+$('audio')[0].play();
+});
+
+$('#play-h').on('click', function () {
+  $('#audio-file').attr('src',
+'Hiding.wav');
+$('audio')[0].play();
+});
+
+$('#play-s').on('click', function () {
+  $('#audio-file').attr('src',
+'Still Looking.mp3');
+$('audio')[0].play();
+});
+
+$('#play-9').on('click', function () {
+  $('#audio-file').attr('src',
+'Memories.mp3');
+$('audio')[0].play();
+});
+
+$('#play-w').on('click', function () {
+  $('#audio-file').attr('src',
+'We Rise.wav');
+$('audio')[0].play();
+});
+
+$('#play-10').on('click', function () {
+  $('#audio-file').attr('src',
+'Endeavors.mp3');
+$('audio')[0].play();
+});
+
+$('#play-11').on('click', function () {
+  $('#audio-file').attr('src',
+'Your Soul.mp3');
+$('audio')[0].play();
+});
+
+$('#play-12').on('click', function () {
+  $('#audio-file').attr('src',
+'Falling.wav');
+$('audio')[0].play();
+});
